4717750315517c5dbefbb148dcbf2dc9f133cd74
Peter Newhook <peter.newhook@gmail.com>
2013-04-13 15:52:16 -0400

    initial commit

---
+++ GraffitiDbJs/js/Model.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+
+(function () {
+    "use strict";
+
+    var dataArray = [
+    {title:"Picture1",artist:"Peter Newhook", img:"Foo"},
+    {title:"Picture2",artist:"Peter Newhook", img:"Foo"},
+    {title:"Picture3",artist:"Peter Newhook", img:"Foo"}
+    ];
+    var tags = new WinJS.Binding.List(dataArray);
+    WinJS.Namespace.define("GraffitiX", {
+        taglist: tags
+});
+})();
---
+++ GraffitiDbJs/js/app.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="ms-appx:///Bing.Maps.JavaScript//js/veapicore.js" />
+/// <reference path="ms-appx://Bing.Maps.JavaScript/js/veapiModules.js" />
+WinJS.Namespace.define("GraffitiDb",
+    {
+        initialize: function() {
+            Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap, culture: 'en-us', homeRegion: 'US', zoom: 18 });
+            function initMap() {
+                var map;
+
+                var mapOptions =
+                {
+                    credentials: "Ap3Ia1YWeZo0uh2LVTF5nH2d03vY-IKtelnVQK77y68WKAzOOhV3VVL_eoiy1eav",
+                    center: new Microsoft.Maps.Location(-33.045904, -71.62056),
+                    zoom: 15
+                };
+
+                map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+            }
+        }
+    }
+);
+
